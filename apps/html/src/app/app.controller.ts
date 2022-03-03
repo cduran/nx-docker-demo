b84b937d227ba94ea417efdcb3e8c8a908dbe968
Carlos Duran <carlos.duran@kyndryl.com>
2022-03-03 07:49:44 -0600

    Demo Nx and Docker working

---
+++ apps/html/src/app/app.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { Todo } from '@test-org/todos';
+import axios from 'axios';
+
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('index')
+  async root() {
+    return {
+      todos: await this.getData(),
+    };
+  }
+
+  async getData() {
+    try {
+      const response = await axios.get<Todo[]>(
+        process.env.apiPath || 'http://localhost:3333'
+      );
+      return response.data;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
---
+++ libs/todos/README.md
@@ -0,0 +1,7 @@
+# todos
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `npx nx test todos` to execute the unit tests via [Jest](https://jestjs.io).
